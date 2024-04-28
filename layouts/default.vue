f0328001be8ebf8304efa7dcd911dcfdb7493067
sigma <seiya.sugimoto.free@gmail.com>
2024-04-28 15:10:57 +0900

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <header class="bg-green-800 shadow">
+      <div class="px-4 py-6 sm:px-6 lg:px-8 flex justify-between items-center">
+        <h1 class="text-3xl font-bold text-gray-100">書籍管理システム</h1>
+        <div class="relative">
+          <img
+            class="h-10 w-10 rounded-full"
+            src="https://via.placeholder.com/150"
+            alt="User Avatar"
+          />
+        </div>
+      </div>
+    </header>
+
+    <div class="flex flex-1">
+      <!-- Left Sidebar -->
+      <div class="w-64 bg-gray-800 text-gray-100">
+        <!-- Global Navigation -->
+        <nav class="mt-8 ml-4">
+          <ul>
+            <li class="py-2 px-4 hover:bg-gray-700">
+              <nuxt-link to="/" class="block">ホーム</nuxt-link>
+            </li>
+            <li class="py-2 px-4 hover:bg-gray-700">
+              <nuxt-link to="/books" class="block">書籍一覧</nuxt-link>
+            </li>
+            <li class="py-2 px-4 hover:bg-gray-700">
+              <nuxt-link to="/authors" class="block">著者一覧</nuxt-link>
+            </li>
+            <li class="py-2 px-4 hover:bg-gray-700">
+              <nuxt-link to="/categories" class="block">カテゴリ一覧</nuxt-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- Page Content -->
+      <main class="flex-1 overflow-y-auto bg-gray-100">
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <slot />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
