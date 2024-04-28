fa8a1a8c2b287c602c6f9b958389513870885ecd
sigma <seiya.sugimoto.free@gmail.com>
2024-04-29 01:18:45 +0900

    add search section

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="container mx-auto px-4 py-8">
+    <div class="flex justify-end">
+      <BooksSearch
+        @searchTitle="onSearchTitle"
+        @searchAuthor="onSearchAuthor"
+        @searchIsbn="onSearchIsbn"
+      />
+    </div>
     <h1 class="text-2xl font-bold">書籍一覧</h1>
     <BooksTable :books="books" />
   </div>
 </template>
 
 <script setup>
-import BooksTable from "./feature/BooksTable.vue";
+import BooksSearch from "@/components/books/BooksSearch.vue";
+import BooksTable from "@/components/books/BooksTable.vue";
 const books = [
   { id: 1, title: "書籍1", author: "著者1" },
   { id: 2, title: "書籍2", author: "著者2" },
@@ -15,4 +23,14 @@ const books = [
   { id: 5, title: "書籍5", author: "著者5" },
   { id: 6, title: "書籍6", author: "著者6" },
 ];
+
+const onSearchTitle = (query) => {
+  console.log(query);
+};
+const onSearchAuthor = (query) => {
+  console.log(query);
+};
+const onSearchIsbn = (query) => {
+  console.log(query);
+};
 </script>
