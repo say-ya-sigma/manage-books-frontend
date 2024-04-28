bb83249498da30ba2a327fc2fa742a79945514db
sigma <seiya.sugimoto.free@gmail.com>
2024-04-29 01:18:15 +0900

    add books search section

---
+++ components/books/BooksSearch.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="mb-4">
+    <div class="flex">
+      <BooksSearchInput
+        id="title"
+        v-model="titleQuery"
+        label="タイトル"
+        placeholder="タイトルで検索"
+      />
+      <button
+        @click="searchTitle"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-4 ml-6 py-2 px-4 rounded"
+      >
+        検索
+      </button>
+    </div>
+
+    <div class="flex">
+      <BooksSearchInput
+        id="author"
+        v-model="authorQuery"
+        label="著者"
+        placeholder="著者で検索"
+      />
+      <button
+        @click="searchAuthor"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-4 ml-6 py-2 px-4 rounded"
+      >
+        検索
+      </button>
+    </div>
+
+    <div class="flex">
+      <BooksSearchInput
+        id="isbn"
+        v-model="isbnQuery"
+        label="ISBN"
+        placeholder="ISBNで検索"
+      />
+      <button
+        @click="searchIsbn"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-4 ml-6 py-2 px-4 rounded"
+      >
+        検索
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BooksSearchInput from "./BooksSearchInput.vue";
+
+const titleQuery = ref("");
+const authorQuery = ref("");
+const isbnQuery = ref("");
+
+const emit = defineEmits(["searchTitle", "searchAuthor", "searchIsbn"]);
+
+const searchTitle = () => {
+  emit("searchTitle", {
+    title: titleQuery.value,
+  });
+};
+
+const searchAuthor = () => {
+  emit("searchAuthor", {
+    author: authorQuery.value,
+  });
+};
+
+const searchIsbn = () => {
+  emit("searchIsbn", {
+    isbn: isbnQuery.value,
+  });
+};
+</script>
---
+++ components/books/BooksSearchInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="block">
+    <label :for="id" class="text-gray-700 font-bold">{{ label }}</label>
+    <div class="relative">
+      <input
+        :id="id"
+        v-model="query"
+        type="text"
+        :placeholder="placeholder"
+        class="w-full pl-4 pr-4 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:border-blue-300 focus:shadow-outline-blue sm:text-sm"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  id: string;
+  label: string;
+  placeholder: string;
+  modelValue?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: "",
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const query = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emit("update:modelValue", value);
+  },
+});
+</script>
