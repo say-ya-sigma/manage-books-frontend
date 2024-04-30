d7dbe3aec5ac48788e1c53f95b286c3a846ab14b
sigma <seiya.sugimoto.free@gmail.com>
2024-05-01 01:45:32 +0900

    apply histories table component

--- pages/books/[bookId].vue
+++ pages/books/[bookId].vue
@@ -18,10 +18,13 @@
         <p class="text-gray-800 leading-relaxed">{{ book.description }}</p>
       </div>
     </div>
+    <HistoriesTable :histories="histories" />
   </div>
 </template>
 
 <script setup lang="ts">
+import HistoriesTable from "@/components/books/detail/HistoriesTable.vue";
+import type { HistoriesTableItem } from "~/components/books/detail/HistoriesTable.type";
 const book = {
   id: 1,
   title: "サンプル書籍",
@@ -32,4 +35,42 @@ const book = {
   description:
     "これはサンプルの書籍詳細です。この書籍は、Tailwind CSSを使ってデザインされたページで表示されています。書籍の詳細情報には、タイトル、著者、ISBNコード、出版社、表紙画像、および説明文が含まれます。",
 };
+
+const histories: HistoriesTableItem[] = [
+  {
+    id: 1,
+    bookNumber: 1,
+    lendStatus: "貸出中",
+    lendDate: "2021-10-01",
+    returnDate: undefined,
+  },
+  {
+    id: 2,
+    bookNumber: 2,
+    lendStatus: "返却済",
+    lendDate: "2021-10-01",
+    returnDate: "2021-10-15",
+  },
+  {
+    id: 3,
+    bookNumber: 3,
+    lendStatus: "貸出中",
+    lendDate: "2021-10-01",
+    returnDate: undefined,
+  },
+  {
+    id: 4,
+    bookNumber: 1,
+    lendStatus: "返却済",
+    lendDate: "2021-09-05",
+    returnDate: "2021-09-22",
+  },
+  {
+    id: 5,
+    bookNumber: 2,
+    lendStatus: "返却済",
+    lendDate: "2021-09-05",
+    returnDate: "2021-09-22",
+  },
+];
 </script>
